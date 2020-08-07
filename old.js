a0bd4ef53aacef877f9f421a25b5910c7af02735
Aidan Kirk <47363073+aidankirk617@users.noreply.github.com>
2020-08-07 12:20:37 -0400

    Testing for Laptop

---
+++ old.js
@@ -0,0 +1,54 @@
+//Journal Free
+
+//Variables
+
+var speechRecognition = window.webkitSpeechRecognition
+
+var recognition = new speechRecognition()
+
+var textbox = $("#textbox")
+
+var instructions = $("#instructions")
+
+var content = ''
+
+recognition.continuous = true
+
+//Recognition Started
+
+recognition.onstart = function () {
+  instructions.text("Voice Recognition is on")
+}
+
+recognition.onspeechend = function () {
+  instructions.text("No Activity")
+}
+
+recognition.onerror = function () {
+  instructions.text("Try Again")
+}
+
+recognition.onresult = function (event) {
+  var current = event.resultIndex;
+
+  var transcript = event.results[current][0].transcript
+
+  content += transcript
+
+  textbox.val(content)
+
+}
+
+$("#start-btn").click(function (event) {
+  if (content.length) {
+    content += ''
+  }
+
+//Initialize
+
+  recognition.start()
+})
+
+textbox.on('input', function (){
+  content = $(this).val()
+})
